8990fb6fa88ad2bd09d6514709c026949f8ba602
PierreTDX <pierre.tondeux@gmail.com>
2025-03-07 12:29:27 +0100

    ajustement composant AccountCard : n° du compte dissocié du titre

--- src/components/AcountCard/index.jsx
+++ src/components/AcountCard/index.jsx
@@ -13,7 +13,7 @@ function AccountCard({ accounts }) {
             {accounts.map((account, index) => (
                 <section className="account" key={index}>
                     <div className="account-content-wrapper">
-                        <h3 className="account-title">{account.title}</h3>
+                        <h3 className="account-title">{account.title} ({account.accountNumber})</h3>
                         <p className="account-amount">${account.amount.toLocaleString("en-US", { minimumFractionDigits: 2 })}</p>
                         <p className="account-amount-description">{account.description}</p>
                     </div>
